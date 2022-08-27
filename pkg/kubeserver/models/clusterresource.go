fc6e1afa25b25f7a13bac868bee901ab493ea3ee
Li Yiyan <lyy0627@sjtu.edu.cn>
2022-08-27 18:22:30 +0800

    refactor(kubeserver): remove redundant code

--- pkg/kubeserver/models/clusterresource.go
+++ pkg/kubeserver/models/clusterresource.go
@@ -10,7 +10,6 @@ import (
 
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	//"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
