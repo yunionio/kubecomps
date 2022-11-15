d79c0b0d15348d28b0c8396e9de4f5d60927d324
Zexi Li <zexi.li@icloud.com>
2022-11-15 14:22:33 +0800

    feat(kubeserver): deploy nginx-ingress-controller

--- pkg/kubeserver/drivers/clusters/kubespray/kubespray.go
+++ pkg/kubeserver/drivers/clusters/kubespray/kubespray.go
@@ -131,6 +131,15 @@ type KubesprayVars struct {
 	DockerHost     string `json:"docker_host"`
 
 	AutoRenewCertificates bool `json:"auto_renew_certificates"`
+
+	// kubernetes-apps
+	IngressNginxEnabled bool `json:"ingress_nginx_enabled"`
+	// IngressNginxControllerImageRepo is "{{ kube_image_repo }}/ingress-nginx/controller" by default
+	IngressNginxControllerImageRepo string `json:"ingress_nginx_controller_image_repo"`
+	// IngressNginxControllerImageTag is:
+	// - k8s v1.17 => v0.41.2
+	// - k8s v1.20 => v1.0.0
+	IngressNginxControllerImageTag string `json:"ingress_nginx_controller_image_tag"`
 }
 
 func (v KubesprayVars) Validate() error {
