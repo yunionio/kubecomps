d1df6b6c98c08972e173e0c1d2bd25c46811666c
Zexi Li <zexi.li@icloud.com>
2023-08-16 00:13:02 +0800

    fix(kubeserver): attach iam role for node

---
+++ pkg/kubeserver/drivers/machines/hypervisors/aws_iam.go
@@ -0,0 +1,361 @@
+package hypervisors
+
+const EC2_ROLE_TRUST_POLICY = `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "ec2.amazonaws.com"},
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}`
+
+// The above policy is also available under: `arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy` as a part of [AWS managed policies
+// for EKS](https://docs.aws.amazon.com/eks/latest/userguide/security-iam-awsmanpol.html).
+const EC2_VPC_CNI_POLICY = `{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AssignPrivateIpAddresses",
+                "ec2:AttachNetworkInterface",
+                "ec2:CreateNetworkInterface",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DescribeInstances",
+                "ec2:DescribeTags",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribeInstanceTypes",
+                "ec2:DetachNetworkInterface",
+                "ec2:ModifyNetworkInterfaceAttribute",
+                "ec2:UnassignPrivateIpAddresses"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags"
+            ],
+            "Resource": [
+                "arn:aws:ec2:*:*:network-interface/*"
+            ]
+        }
+    ]
+}`
+
+// https://cloud-provider-aws.sigs.k8s.io/prerequisites/
+const EC2_CLOUD_PROVIDER_POLICY = `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}`
+
+// https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.6/deploy/installation/#option-b-attach-iam-policies-to-nodes
+// https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.6.0/docs/install/iam_policy.json
+const EC2_LB_CONTROLLER_POLICY = `{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": "elasticloadbalancing.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeAddresses",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeInternetGateways",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeVpcPeeringConnections",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeInstances",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribeTags",
+                "ec2:GetCoipPoolUsage",
+                "ec2:DescribeCoipPools",
+                "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:DescribeLoadBalancerAttributes",
+                "elasticloadbalancing:DescribeListeners",
+                "elasticloadbalancing:DescribeListenerCertificates",
+                "elasticloadbalancing:DescribeSSLPolicies",
+                "elasticloadbalancing:DescribeRules",
+                "elasticloadbalancing:DescribeTargetGroups",
+                "elasticloadbalancing:DescribeTargetGroupAttributes",
+                "elasticloadbalancing:DescribeTargetHealth",
+                "elasticloadbalancing:DescribeTags"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cognito-idp:DescribeUserPoolClient",
+                "acm:ListCertificates",
+                "acm:DescribeCertificate",
+                "iam:ListServerCertificates",
+                "iam:GetServerCertificate",
+                "waf-regional:GetWebACL",
+                "waf-regional:GetWebACLForResource",
+                "waf-regional:AssociateWebACL",
+                "waf-regional:DisassociateWebACL",
+                "wafv2:GetWebACL",
+                "wafv2:GetWebACLForResource",
+                "wafv2:AssociateWebACL",
+                "wafv2:DisassociateWebACL",
+                "shield:GetSubscriptionState",
+                "shield:DescribeProtection",
+                "shield:CreateProtection",
+                "shield:DeleteProtection"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:RevokeSecurityGroupIngress"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateSecurityGroup"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags"
+            ],
+            "Resource": "arn:aws:ec2:*:*:security-group/*",
+            "Condition": {
+                "StringEquals": {
+                    "ec2:CreateAction": "CreateSecurityGroup"
+                },
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags",
+                "ec2:DeleteTags"
+            ],
+            "Resource": "arn:aws:ec2:*:*:security-group/*",
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:RevokeSecurityGroupIngress",
+                "ec2:DeleteSecurityGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:CreateTargetGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:CreateListener",
+                "elasticloadbalancing:DeleteListener",
+                "elasticloadbalancing:CreateRule",
+                "elasticloadbalancing:DeleteRule"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:RemoveTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+            ],
+            "Condition": {
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:RemoveTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:listener/net/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener/app/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*/*",
+                "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                "elasticloadbalancing:SetIpAddressType",
+                "elasticloadbalancing:SetSecurityGroups",
+                "elasticloadbalancing:SetSubnets",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:ModifyTargetGroup",
+                "elasticloadbalancing:ModifyTargetGroupAttributes",
+                "elasticloadbalancing:DeleteTargetGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags"
+            ],
+            "Resource": [
+                "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+                "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "elasticloadbalancing:CreateAction": [
+                        "CreateTargetGroup",
+                        "CreateLoadBalancer"
+                    ]
+                },
+                "Null": {
+                    "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:DeregisterTargets"
+            ],
+            "Resource": "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:SetWebAcl",
+                "elasticloadbalancing:ModifyListener",
+                "elasticloadbalancing:AddListenerCertificates",
+                "elasticloadbalancing:RemoveListenerCertificates",
+                "elasticloadbalancing:ModifyRule"
+            ],
+            "Resource": "*"
+        }
+    ]
+}`
--- pkg/utils/awscli/awscli.go
+++ pkg/utils/awscli/awscli.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/log"
@@ -64,6 +65,14 @@ func (cli *AwsCliRC) getCmd(args ...string) *exec.Cmd {
 }
 
 func (cli *AwsCliRC) execute(args ...string) error {
+	output, err := cli.executeWithOutput(args...)
+	if err != nil {
+		return errors.Wrapf(err, "output: %s", output)
+	}
+	return nil
+}
+
+func (cli *AwsCliRC) executeWithOutput(args ...string) ([]byte, error) {
 	cmd := cli.getCmd(args...)
 	env := os.Environ()
 	env = append(env, cli.ToEnv()...)
@@ -72,9 +81,23 @@ func (cli *AwsCliRC) execute(args ...string) error {
 	log.Debugf("run cmd: %s, with env: %v", cmd.String(), cmd.Env)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return errors.Wrapf(err, "output: %s", output)
+		return nil, errors.Wrapf(err, "output: %s\ncmd: %s", output, cmd.String())
 	}
-	return nil
+	return output, nil
+}
+
+func (cli *AwsCliRC) executeWithJSON(args ...string) (jsonutils.JSONObject, error) {
+	nArgs := []string{"--output", "json"}
+	nArgs = append(nArgs, args...)
+	output, err := cli.executeWithOutput(nArgs...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "output: %s", output)
+	}
+	obj, err := jsonutils.Parse(output)
+	if err != nil {
+		return nil, errors.Wrapf(err, "jsonutils.Parse with %s", output)
+	}
+	return obj, nil
 }
 
 func (cli *AwsCliRC) IAMCreateRole(roleName string, policyContent string) error {
@@ -83,6 +106,79 @@ func (cli *AwsCliRC) IAMCreateRole(roleName string, policyContent string) error 
 		return errors.Wrapf(err, "write policy file: %s", fn)
 	}
 	return cli.execute("iam", "create-role", "--role-name", roleName, "--assume-role-policy-document", fmt.Sprintf("file://%s", fn))
+}
+
+type Policy struct {
+	PolicyName                    string    `json:"PolicyName"`
+	PolicyId                      string    `json:"PolicyId"`
+	Arn                           string    `json:"Arn"`
+	Path                          string    `json:"Path"`
+	DefaultVersionId              string    `json:"DefaultVersionId"`
+	AttachmentCount               int       `json:"AttachmentCount"`
+	PermissionsBoundaryUsageCount int       `json:"PermissionsBoundaryUsageCount"`
+	IsAttachable                  bool      `json:"IsAttachable"`
+	CreateDate                    time.Time `json:"CreateDate"`
+	UpdateDate                    time.Time `json:"UpdateDate"`
+}
+
+func (cli *AwsCliRC) IAMListPolicies(policyName string, pathPrefix string) ([]Policy, error) {
+	args := []string{
+		"iam", "list-policies",
+	}
+	if policyName != "" {
+		args = append(args, []string{"--query", fmt.Sprintf("Policies[?PolicyName==`%s`]", policyName)}...)
+	}
+	if pathPrefix != "" {
+		args = append(args, []string{"--path-prefix", pathPrefix}...)
+	}
+	out, err := cli.executeWithJSON(args...)
+	if err != nil {
+		return nil, err
+	}
+	ret := []Policy{}
+	if err := out.Unmarshal(&ret); err != nil {
+		return nil, errors.Wrapf(err, "Unmarshal %s to []Policy", out.String())
+	}
+	return ret, nil
+}
+
+func (cli *AwsCliRC) IAMCreatePolicy(policyName string, policyContent string, pathName string) (*Policy, error) {
+	fn := fmt.Sprintf("/tmp/aws-%s-policy.json", policyName)
+	if err := os.WriteFile(fn, []byte(policyContent), 0644); err != nil {
+		return nil, errors.Wrapf(err, "write policy file: %s", fn)
+	}
+
+	args := []string{
+		"iam", "create-policy", "--policy-name", policyName, "--policy-document", fmt.Sprintf("file://%s", fn),
+	}
+	if len(pathName) != 0 {
+		args = append(args, "--path", pathName)
+	}
+	obj, err := cli.executeWithJSON(args...)
+	if err != nil {
+		return nil, err
+	}
+	policy := new(Policy)
+	if err := obj.Unmarshal(policy, "Policy"); err != nil {
+		return nil, errors.Wrapf(err, "Unmarshal %s to Policy", obj.String())
+	}
+	return policy, nil
+}
+
+func (cli *AwsCliRC) IAMEnsurePolicy(policyName string, policyContent string, pathName string) (*Policy, error) {
+	policies, err := cli.IAMListPolicies(policyName, pathName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "IAMListPolicies")
+	}
+	if len(policies) != 0 {
+		return &policies[0], nil
+	}
+	// create policy
+	return cli.IAMCreatePolicy(policyName, policyContent, pathName)
+}
+
+func (cli *AwsCliRC) IAMAttachRolePolicy(roleName string, policyArn string) error {
+	return cli.execute("iam", "attach-role-policy", "--role-name", roleName, "--policy-arn", policyArn)
 }
 
 func (cli *AwsCliRC) IAMPutRolePolicy(roleName string, policyName string, policyContent string) error {
@@ -102,7 +198,20 @@ func (cli *AwsCliRC) IAMAddRoleToInstanceProfile(profileName string, roleName st
 }
 
 func (cli *AwsCliRC) EC2AssociateIAMInstanceProfile(instanceId string, profileName string) error {
-	return cli.execute("ec2", "associate-iam-instance-profile", "--instance-id", instanceId, "--iam-instance-profile", fmt.Sprintf("Name='%s'", profileName))
+	f := func() error {
+		return cli.execute("ec2", "associate-iam-instance-profile", "--instance-id", instanceId, "--iam-instance-profile", fmt.Sprintf("Name='%s'", profileName))
+	}
+
+	var err error
+	for i := 0; i < 2; i++ {
+		err = f()
+		if err == nil {
+			return nil
+		}
+		// 关联第一次创建的 InstanceProfile 要等一段时间才生效
+		time.Sleep(15 * time.Second)
+	}
+	return err
 }
 
 type Tag struct {
@@ -126,6 +235,7 @@ const (
 
 const (
 	ClusterPrefixKey = "kubernetes.io/cluster"
+	SubnetRoleELBKey = "kubernetes.io/role/elb"
 )
 
 func NewClusterTag(clusterName string, cv ClusterTagValue) *Tag {
